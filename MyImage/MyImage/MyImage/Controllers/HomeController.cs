1b309c84e149022dce0f77d2131aa0a5c9eb1318
Mujtabaa-ahmed <mujtaba56383@gmail.com>
2024-05-09 17:39:10 +0500

    profile--continuning

--- MyImage/MyImage/MyImage/Controllers/HomeController.cs
+++ MyImage/MyImage/MyImage/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using MyImage.DB_Context;
 using MyImage.Models;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 using static MyImage.Models.class_accounts;
 
 namespace MyImage.Controllers
@@ -23,22 +24,34 @@ namespace MyImage.Controllers
         [HttpPost]
         public IActionResult Logining()
         {
-            var e_mail = Request.Form["email"].FirstOrDefault();
+
+            var email = Request.Form["email"].FirstOrDefault();
             var password = Request.Form["password"].FirstOrDefault();
-            List<class_accounts> user = database.Accounts.Where(a => a.e_mail == e_mail && a.password == password).ToList();
+
+            List<class_user_table> user_details = database.user_tables.Where(a => a.e_mail == email).ToList();
+            List<class_accounts> user = database.Accounts.Where(a => a.e_mail == email && a.password == password).ToList();
+
+
             if (user.Count != 0)
             {
-                HttpContext.Session.SetString("session_Id", user[0].signin_id.ToString());
-                HttpContext.Session.SetString("session_first_name", user[0].first_name.ToString());
-                HttpContext.Session.SetString("session_last_name", user[0].last_name.ToString());
-                HttpContext.Session.SetString("session_email", user[0].e_mail.ToString());
+                
+                HttpContext.Session.SetString("session_Id", user_details[0].costumer_id.ToString());
+                HttpContext.Session.SetString("session_first_name", user_details[0].f_name.ToString());
+                HttpContext.Session.SetString("session_last_name", user_details[0].l_name.ToString());
+                HttpContext.Session.SetString("session_email", user_details[0].gander.ToString());
+                HttpContext.Session.SetString("session_email", user_details[0].dob.ToString());
+                HttpContext.Session.SetString("session_email", user_details[0].p_number.ToString());
+                HttpContext.Session.SetString("session_email", user_details[0].addres.ToString());
+                HttpContext.Session.SetString("session_email", user_details[0].e_mail.ToString());
 
-                Class_session.user_id = user[0].signin_id.ToString();
-                Class_session.user_fname = user[0].first_name.ToString();
-                Class_session.user_lname = user[0].last_name.ToString();
-                Class_session.user_email = user[0].e_mail.ToString();
-
-
+                Class_session.user_id = user_details[0].costumer_id.ToString();
+                Class_session.user_fname = user_details[0].f_name.ToString();
+                Class_session.user_lname = user_details[0].l_name.ToString();
+                Class_session.gander = user_details[0].gander.ToString();
+                Class_session.dateOB = user_details[0].dob.ToString();
+                Class_session.number = user_details[0].p_number.ToString();
+                Class_session.adders = user_details[0].addres.ToString();
+                Class_session.user_email = user_details[0].e_mail.ToString();
                 return Content("Loged In");
             }
             else 
@@ -66,39 +79,54 @@ namespace MyImage.Controllers
 
             var first_name = Request.Form["first"];
             var last_name = Request.Form["last"];
-            var e_mail = Request.Form["mail"];
+            var e_mail = Request.Form["mail"].ToString();
             var password = Request.Form["password"];
-            
-            class_accounts signups = new class_accounts() 
+
+            List<class_accounts> valid = database.Accounts.Where(a => a.e_mail == e_mail).ToList();
+
+            if (valid.Count == 0)
             {
-                first_name = first_name,
-                last_name = last_name,
-                e_mail = e_mail,
-                password = password
-            };
+                class_accounts signups = new class_accounts()
+                {
+                    first_name = first_name,
+                    last_name = last_name,
+                    e_mail = e_mail,
+                    password = password
+                };
 
 
 
-            List<class_accounts> getId = new List<class_accounts>();
-            var gtt = getId.FirstOrDefault(a => a.first_name == first_name);
+                List<class_accounts> getId = new List<class_accounts>();
+                var gtt = getId.FirstOrDefault(a => a.first_name == first_name);
 
-            class_user_table user = new class_user_table()
+                class_user_table user = new class_user_table()
+                {
+                    f_name = first_name,
+                    l_name = last_name,
+                    addres = "-",
+                    dob = "-",
+                    gander = "-",
+                    p_number = 00000,
+                    e_mail = e_mail
+                };
+
+
+                database.Add(signups);
+                database.SaveChanges();
+                database.Add(user);
+                database.SaveChanges();
+                return Content("");
+            }
+
+            else
             {
-                f_name = first_name,
-                l_name = last_name,
-                addres = "-",
-                dob = "-",
-                gander = "-",
-                p_number = 00000,
-                e_mail = e_mail
-            };
-
-
-            database.Add(signups);
-            database.SaveChanges();
-            database.Add(user);
-            database.SaveChanges();
-            return Content("bhai bht aala");
+                return Content("Email is already registered");
+            }
+                
+        }
+        public IActionResult edit_profile() 
+        {
+            return View();
         }
         public IActionResult Index()
         {
--- MyImage/MyImage/MyImage/Models/class_session.cs
+++ MyImage/MyImage/MyImage/Models/class_session.cs
@@ -6,5 +6,10 @@
         public static string user_fname { get; set; } = string.Empty;
         public static string user_lname { get; set; } = string.Empty;
         public static string user_email { get; set; } = string.Empty;
+        public static string gander { get; set; } = string.Empty;
+        public static string dateOB { get; set; } = string.Empty;
+        public static string number { get; set; } = string.Empty;
+        public static string adders { get; set; } = string.Empty;
+       
     }
 }
